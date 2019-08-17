0ffcb809e901e7d33d7e89cbaeb8e954e668c704
Alan Leverenz <awleverenz@aol.com>
2019-08-17 13:51:19 -0400

    set the query function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,26 @@
 $(document).ready(function() {
 
-var queryURL = "";
+var apiKey = "GAN5Vuqp6dyl6vNHxlmwbLizhaZMVVf6";
+var search = "Pelosi";
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=" + apiKey;
 
-    $.ajax({
+
+var searchNYT = function search () {
+
+   $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
 
         console.log(response);
+      });
+    };
+});
+
+searchNYT();
+
+console.log(response);
 
 
 
-}
+
