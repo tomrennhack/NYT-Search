6547d97f5a1f5869666d9614304c183fc3c06736
Alan Leverenz <awleverenz@aol.com>
2019-08-21 14:42:07 -0400

    start and end date queries are functioning. Fixed publication date by formatting it with 'new Date()'.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,21 +22,19 @@ $(document).ready(function() {
     var search = $("#search-term").val().trim();
     var records = $("#number-of-records").val();
     var sYear = $("#start-yr").val().trim();
-    var startYear = sYear + "0101";
-    console.log("start year: " + startYear);
+    var startYear = sYear.length > 0 ? "&start_date=" + sYear + "0101" : "";
     var eYear = $("#end-yr").val().trim();
-    var endYear = eYear + "1231";
-    console.log("end year: " + endYear);
+    var endYear = eYear.length > 0 ? "&end_date=" + eYear + "1231" : "";
 
   // build queryURL
   var apiKey = "GAN5Vuqp6dyl6vNHxlmwbLizhaZMVVf6";
-  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&begin_date=" + startYear + "&end_date=" + endYear + "&api-key=" + apiKey;
-  
+  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + startYear + endYear + "&api-key=" + apiKey;
+    console.log(queryURL);
    $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(results) {
-        console.log(queryURL);
+
       for ( var i = 0 ; i < records ; i++ ) {
 
         //  keys to capture
@@ -54,7 +52,11 @@ $(document).ready(function() {
         var leadPara = results.response.docs[i].lead_paragraph;
         var pLeadPara = $("<p>").html("<b>Lead Paragraph</b>: " + leadPara);
 
-        var pubDate = Date(results.response.docs[i].pub_date);
+        var webURL = results.response.docs[i].web_url;
+        var aWebURL = "<a href='" + webURL + "' target='_blank'>" + headline + "</a>";
+        var pWebURL = $("<p>").html("<b>Web URL</b>: " + aWebURL);
+
+        var pubDate = new Date(results.response.docs[i].pub_date);
         var pPubDate = $("<p>").html("<b>Publication Date</b>: " + pubDate);
 
         var section = results.response.docs[i].section_name;
@@ -63,14 +65,10 @@ $(document).ready(function() {
         var subsection = results.response.docs[i].subsection_name;
         var pSubsection = $("<p>").html("<b>Subsection</b>: " + subsection);
 
-        var webURL = results.response.docs[i].web_url;
-        var aWebURL = "<a href='" + webURL + "' target='_blank'>" + headline + "</a>";
-        var pWebURL = $("<p>").html("<b>Web URL</b>: " + aWebURL);
-
         var divider = $("<hr>");
 
         // append to the top-articles div
-        topArticlesDiv.append(pHeadline).append(pByline).append(pAbstract).append(pLeadPara).append(pSection).append(pSubsection).append(pWebURL).append(pPubDate).append(divider);
+        topArticlesDiv.append(pHeadline).append(pByline).append(pPubDate).append(pAbstract).append(pLeadPara).append(pSection).append(pSubsection).append(pWebURL).append(divider);
 
         }; // end for loop
       }); // end then
